36e378af78beb6864a2d2f94cb41eeba17933dee
JAYAPRIYA T <jayapriyaanandkumar@gmail.com>
2024-06-05 10:26:20 +0530

    sucessfully completed front end phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import ManageRoom from "./components/ManageRoom";
+import UniversityExam from "./components/UniversityExam";
+import SeatAllocation from "./components/SeatAllocation";
+import { Route, Routes } from 'react-router-dom';
+import RequireAuth from "./components/RequireAuth";
+import PersistLogin from "./components/PersistLogin";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {/* public routes */}
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Protection needed routes */}
+      <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Layout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="manage-room" element={<ManageRoom />} />
+            <Route path="university-exam" element={<UniversityExam />} />
+            <Route path="seat-allocation" element={<SeatAllocation />} />
+          </Route>
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
